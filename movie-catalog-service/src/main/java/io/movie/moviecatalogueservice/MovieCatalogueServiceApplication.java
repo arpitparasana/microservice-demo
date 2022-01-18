7d87e0c4497e06d93931b67d58ff11344a270fa9
arpitparasana <au.parasana@gmail.com>
2022-01-17 21:47:39 -0500

    Add rest template support + api call to movie-info service

--- movie-catalog-service/src/main/java/io/movie/moviecatalogueservice/MovieCatalogueServiceApplication.java
+++ movie-catalog-service/src/main/java/io/movie/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -2,11 +2,18 @@ package io.movie.moviecatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogueServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
--- movie-catalog-service/src/main/java/io/movie/moviecatalogueservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/movie/moviecatalogueservice/resource/MovieCatalogResource.java
@@ -1,21 +1,37 @@
 package io.movie.moviecatalogueservice.resource;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.movie.moviecatalogueservice.model.CatalogItem;
+import io.movie.moviecatalogueservice.model.Movie;
+import io.movie.moviecatalogueservice.model.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		return Collections.singletonList(
-				new CatalogItem("Transformers", "Awesome Movie", 10));
+		
+		List<Rating> ratings = Arrays.asList(
+				new Rating("tr", 10), new Rating("lotr", 9)
+		);
+				
+		return ratings.stream().map(rating ->  {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getMovieName(), "Awesome movie", rating.getRating());
+		}).collect(Collectors.toList());
+		
 	}
 }
