527da6398944b5dd35cbe592fac0facaeab9c036
arpitparasana <au.parasana@gmail.com>
2022-01-18 11:22:27 -0500

    Add getUserRating endpoint

--- ratings-info-service/src/main/java/io/movie/ratingsinfoservice/resource/RatingsInfoResource.java
+++ ratings-info-service/src/main/java/io/movie/ratingsinfoservice/resource/RatingsInfoResource.java
@@ -1,10 +1,14 @@
 package io.movie.ratingsinfoservice.resource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.movie.ratingsinfoservice.model.Rating;
+import io.movie.ratingsinfoservice.model.UserRating;
 
 @RestController
 @RequestMapping("/ratings")
@@ -12,8 +16,14 @@ public class RatingsInfoResource {
 
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
-		
 		return new Rating(movieId, 10);
-		
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(new Rating("tr", 10), new Rating("lotr", 9));
+		UserRating userRating = new UserRating();
+		userRating.setRatings(ratings);
+		return userRating;
 	}
 }
