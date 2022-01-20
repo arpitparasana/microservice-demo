f4e82b5654885481bc3afebe41fc4c6656e90b74
arpitparasana <au.parasana@gmail.com>
2022-01-20 17:45:29 -0500

    Add Hystrix to handle faults (circuit breaker)

--- movie-catalog-service/src/main/java/io/movie/moviecatalogueservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/movie/moviecatalogueservice/resource/MovieCatalogResource.java
@@ -1,5 +1,6 @@
 package io.movie.moviecatalogueservice.resource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import io.movie.moviecatalogueservice.model.CatalogItem;
 import io.movie.moviecatalogueservice.model.Movie;
@@ -24,6 +27,7 @@ public class MovieCatalogResource {
 //	private WebClient.Builder webClientBuilder;
 
 	@RequestMapping("/{userId}")
+	@HystrixCommand(fallbackMethod = "getFallBackCatalog")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
 		UserRating userRatings = 
@@ -34,7 +38,7 @@ public class MovieCatalogResource {
 		return userRatings.getRatings().stream().map(rating -> {
 			Movie movie = restTemplate.getForObject("http://movie-info/movies/" + rating.getMovieId(), Movie.class);
 
-			/*
+			/* Alternative WebClient way
 			 * Movie movie = webClientBuilder.build() .get()
 			 * .uri("http://localhost:8082/movies/" + rating.getMovieId()) .retrieve()
 			 * .bodyToMono(Movie.class) .block();
@@ -43,5 +47,10 @@ public class MovieCatalogResource {
 			return new CatalogItem(movie.getMovieName(), "Awesome movie", rating.getRating());
 		}).collect(Collectors.toList());
 
+	}
+	
+	public List<CatalogItem> getFallBackCatalog(@PathVariable("userId") String userId) {
+		return Arrays.asList(new CatalogItem("No movie", "", 1));
+	
 	}
 }
